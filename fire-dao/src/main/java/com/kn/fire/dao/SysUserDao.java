190a51070e42353972bd2dbe8ccdc0212ec52095
yskcoder <simplestperson@163.com>
2019-12-25 16:04:03 +0800

    #添加generator和tk.mapper

---
+++ fire-dao/src/main/java/com/kn/fire/dao/SysUserDao.java
@@ -0,0 +1,20 @@
+package com.kn.fire.dao;
+
+import com.kn.fire.model.SysUser;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户表数据处理类
+ *
+ * @author:       yskcoder
+ * @createDate:   2019/12/25 16:01
+ * @version:      1.0
+*/
+@Mapper
+public interface SysUserDao extends tk.mybatis.mapper.common.Mapper<SysUser> {
+
+}
+
+
+
+
---
+++ fire-dao/src/main/java/com/kn/fire/model/SysUser.java
@@ -0,0 +1,107 @@
+package com.kn.fire.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+
+@Table(name = "`sys_user`")
+public class SysUser {
+    private Long userId;
+
+    private String avatar;
+
+    private String account;
+
+    private String salt;
+
+    private String password;
+
+    private String name;
+
+    private Short sex;
+
+    private String email;
+
+    private String phone;
+
+    private LocalDateTime createTime;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar == null ? null : avatar.trim();
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account == null ? null : account.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt == null ? null : salt.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Short getSex() {
+        return sex;
+    }
+
+    public void setSex(Short sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+}
