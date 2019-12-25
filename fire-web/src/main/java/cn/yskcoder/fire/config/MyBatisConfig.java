d3ce710fcb42d2f23539ad088bef2c6c67fd09ee
yskcoder <simplestperson@163.com>
2019-12-25 16:21:04 +0800

    #添加generator和tk.mapper

---
+++ fire-web/src/main/java/cn/yskcoder/fire/config/MyBatisConfig.java
@@ -0,0 +1,36 @@
+package cn.yskcoder.fire.config;
+
+import cn.yskcoder.fire.config.properties.DruidProperties;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * mybatisConfig配置类
+ *
+ * @author:       yskcoder
+ * @createDate:   2019/12/25 16:20
+ * @version:      1.0
+*/
+@Configuration
+public class MyBatisConfig {
+    @Autowired
+    DruidProperties druidProperties;
+
+
+    /**
+     * Fire数据源
+     */
+    private DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        druidProperties.config(dataSource);
+        return dataSource;
+    }
+
+    @Bean
+    public DruidDataSource datasource() {
+        return dataSource();
+    }
+}
